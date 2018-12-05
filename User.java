b45a4fc5c9f16ebab5900531b3fa0c978b4bee53
wu <wudongjun31@qq.com>
2018-12-05 11:39:03 +0800

    add a userbean

---
+++ User.java
@@ -0,0 +1,34 @@
+package com.offcn.po;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="user")
+public class User {
+private String userid;
+private String name;
+private String password;
+public String getUserid() {
+	return userid;
+}
+public void setUserid(String userid) {
+	this.userid = userid;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+@Override
+public String toString() {
+	return "User [userid=" + userid + ", name=" + name + ", password=" + password + "]";
+}
+
+
+}
